a73ab70eff52c63824336f0fdf8fc5ffa0ccc3ce
Saima Khan <tithi888khan@gmail.com>
2021-03-24 01:02:14 +0600

    Strings: Justifier

--- uri-1273.py
+++ uri-1273.py
@@ -1,18 +1,50 @@
-while(1):
-    num=int(input())
-    if num==0:
+firstTestCase = True
+while (True):
+    
+    num = int(input())
+    if num == 0:
         break
-    initial = 0
-    texts=[]
-    for i in range (num):
-        text=input()
+    
+    if not firstTestCase:
+        print("")
+
+    texts = []
+    maxLength = 0
+
+    for i in range(num):
+        text = input()
         texts.append(text)
-        if len(text)>initial:
-            initial=len(text)
+
+        if len(text) > maxLength:
+            maxLength = len(text)
+
     for word in texts:
-        updated=''
-        spaces = initial - len(word)
-        for i in range (spaces):
-            updated+=' '
-        updated+=word
-        print(updated)
+        spaces = maxLength - len(word)
+        updatedWord = ' ' * spaces + word
+        print(updatedWord)
+
+    firstTestCase = False
+
+
+    
+
+
+
+#while(1):
+#     num=int(input())
+#     if num==0:
+#         break
+#     initial = 0
+#     texts=[]
+#     for i in range (num):
+#         text=input()
+#         texts.append(text)
+#         if len(text)>initial:
+#             initial=len(text)
+#     for word in texts:
+#         updated=''
+#         spaces = initial - len(word)
+#         for i in range (spaces):
+#             updated+=' '
+#         updated+=word
+#         print(updated)
